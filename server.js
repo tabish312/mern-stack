739021c69f8c329315731f7964ac49a34913b0d3
Tabish Ali <mr.tabish1@gmail.co>
2023-01-04 03:54:56 +0500

    Changes

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("main");
+const express = require("express");
+const app = express();
+const dotenv = require('dotenv').config()
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json(
+        { "status": "server is working" }
+    );
+})
+
+
+const port = process.env.PORT;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
